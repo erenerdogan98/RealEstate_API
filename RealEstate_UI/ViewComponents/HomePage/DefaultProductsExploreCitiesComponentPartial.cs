f0754df96d835354f13458fa58a00cd58a3a412a
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-02-28 16:55:11 +0300

    ..

--- RealEstate_UI/ViewComponents/HomePage/DefaultProductsExploreCitiesComponentPartial.cs
+++ RealEstate_UI/ViewComponents/HomePage/DefaultProductsExploreCitiesComponentPartial.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+
 namespace RealEstate_UI.ViewComponents.HomePage
 {
     public class DefaultProductsExploreCitiesComponentPartial : ViewComponent
     {
         public IViewComponentResult Invoke()
         {
-            return View();
+           return View();
         }
     }
 }
+
