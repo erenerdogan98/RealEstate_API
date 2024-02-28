e52f1a92855534384469f9d9ed1952f13bc1cd74
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-02-28 18:07:11 +0300

    added Dtos for WhoWeAre , and added model for take values for using in View , Now , can consume our API .

---
+++ RealEstate_UI/Models/WhoWeAreComponentModel.cs
@@ -0,0 +1,10 @@
+﻿namespace RealEstate_UI.Models
+{
+    public class WhoWeAreComponentModel
+    {
+        public string? Title { get; set; } 
+        public string? Subtitle { get; set; }
+        public string? Description { get; set; }
+        public string? Description2 { get; set; }
+    }
+}
--- RealEstate_UI/ViewComponents/HomePage/DefaultWhoWeAreComponentPartial.cs
+++ RealEstate_UI/ViewComponents/HomePage/DefaultWhoWeAreComponentPartial.cs
@@ -1,11 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_UI.Dtos.WhoWeAreDtos;
+using RealEstate_UI.Models;
 
 namespace RealEstate_UI.ViewComponents.HomePage
 {
-    public class DefaultWhoWeAreComponentPartial : ViewComponent
+    public class DefaultWhoWeAreComponentPartial(IHttpClientFactory httpClientFactory) : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+        public async Task<IViewComponentResult> InvokeAsync()
         {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44349/api/WhoWeAreDetail/WhoWeAreDetails");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<List<ResultWhoWeAreDetailDto>>(jsonData);
+
+                if (value != null && value.Any())
+                {
+                    var model = new WhoWeAreComponentModel
+                    {
+                        Title = value.Select(x => x.Title).FirstOrDefault(),
+                        Subtitle = value.Select(x => x.SubTitle).FirstOrDefault(),
+                        Description = value.Select(x => x.Description).FirstOrDefault(),
+                        Description2 = value.Select(x => x.Description2).FirstOrDefault()
+                    };
+
+                    return View(model);
+                }
+            }
             return View();
         }
     }
