6cad49e53a50bbf4b7c701e5b9aadcfa6363a95e
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-02-28 04:07:56 +0300

    installed packate for Dapper , added connection string and Context class in Models Folder ..

---
+++ RealEstate_API/Models/DapperContext/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace RealEstate_API.Models.DapperContext
+{
+    public class Context(IConfiguration configuration)
+    {
+        private readonly IConfiguration _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        private readonly string _connectionString = configuration.GetConnectionString("DefaulConnection") ?? throw new ArgumentNullException("Cannot Connect");
+
+        public IDbConnection GetConnection() => new SqlConnection(_connectionString);
+    }
+}
