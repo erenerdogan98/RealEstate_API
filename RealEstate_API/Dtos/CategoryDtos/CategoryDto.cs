e864fbd9e5c4fa0314ac019e513158419c4d3dbf
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-02-28 04:09:10 +0300

    added for Dto s ..

---
+++ RealEstate_API/Dtos/CategoryDtos/CategoryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RealEstate_API.Dtos.CategoryDtos
+{
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool Status { get; set; } = false;
+    }
+}
---
+++ RealEstate_API/Dtos/CategoryDtos/CreateCategoryDto.cs
@@ -0,0 +1,7 @@
+﻿namespace RealEstate_API.Dtos.CategoryDtos
+{
+    public class CreateCategoryDto
+    {
+        public string? Name { get; set; }
+    }
+}
