9285c6305ac9117a69287cd975f164617f889afa
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-02-28 04:13:47 +0300

    added method which have in DependencyManager class for dependency injection ..

--- RealEstate_API/Program.cs
+++ RealEstate_API/Program.cs
@@ -1,3 +1,5 @@
+using RealEstate_API.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.ConfigureMyServices();
 
 var app = builder.Build();
 
