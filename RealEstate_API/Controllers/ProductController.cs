1a1a92c112407c9e7440d4dfd89ad20eb1e876fe
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-02-28 04:12:18 +0300

    Added controller for product  , added two methods (ProductList and ProductListWithCategory) ..

---
+++ RealEstate_API/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_API.Repositories.Abstract;
+
+namespace RealEstate_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController(IProductRepository productRepository) : ControllerBase
+    {
+        private readonly IProductRepository _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+
+        [HttpGet("productlist")]
+        public async Task<IActionResult> ProductList()
+        {
+            var products = await _productRepository.GetAllAsync() ?? throw new InvalidOperationException("Products not found");
+            return Ok(products);
+        }
+        [HttpGet("ProductListWithCategory")]
+        public async Task<IActionResult> ProductListWithCategory()
+        {
+            var products = await _productRepository.GetProductsWithCategory() ?? throw new ArgumentNullException("No products found for this category.");
+            return Ok(products);
+        }
+
+    }
+}
