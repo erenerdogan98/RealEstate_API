d4e9117b3c0d2f3e19b50f2bf47c8cfac8f32f7f
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-02-29 07:25:16 +0300

    Added Dtos for Service table

---
+++ RealEstate_API/Dtos/ServiceDtos/CreateServiceDto.cs
@@ -0,0 +1,8 @@
+﻿namespace RealEstate_API.Dtos.ServiceDtos
+{
+    public class CreateServiceDto
+    {
+        public string? Name { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ RealEstate_API/Dtos/ServiceDtos/ServiceDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RealEstate_API.Dtos.ServiceDtos
+{
+    public class ServiceDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool Status { get; set; }
+    }
+}
