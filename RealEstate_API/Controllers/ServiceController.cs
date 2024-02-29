928056e44ef103910f6b6844ee6cdc821eba6d92
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-02-29 07:26:53 +0300

    added controller for Service , and completed CRUD operations ..

---
+++ RealEstate_API/Controllers/ServiceController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_API.Dtos.CategoryDtos;
+using RealEstate_API.Dtos.ServiceDtos;
+using RealEstate_API.Repositories.Abstract;
+using RealEstate_API.Repositories.CategoryRepository;
+
+namespace RealEstate_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceController(IServiceRepository serviceRepository) : ControllerBase
+    {
+        private readonly IServiceRepository _serviceRepository = serviceRepository ?? throw new ArgumentNullException(nameof(serviceRepository));
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var values = await _serviceRepository.GetAllAsync();
+            return Ok(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateService(CreateServiceDto createServiceDto)
+        {
+            if (createServiceDto == null)
+            {
+                // Eğer categoryDto null ise BadRequest döndür
+                return BadRequest("Invalid category data");
+            }
+            try
+            {
+                await _serviceRepository.CreateServiceAsync(createServiceDto);
+                return Ok("Service added!");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error adding category: {ex.Message}");
+            }
+        }
+    }
+}
