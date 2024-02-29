72bf7c19def2015f05c7c1ec7dddfd4b0cd3e36f
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-02-29 18:00:01 +0300

    Added class for Dto bottom grid , added its namespace to view imports , changed names for DefaultServices.. to DefaultBottomGrid (also in index , changed to.. ).. , now can Consume our API ..

---
+++ RealEstate_UI/ViewComponents/HomePage/DefaultBottomGridComponentPartial.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_UI.Dtos.BottomGridDtos;
+using System.Net.Http;
+
+namespace RealEstate_UI.ViewComponents.HomePage
+{
+    public class DefaultBottomGridComponentPartial(IHttpClientFactory httpClientFactory) : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44349/api/BottomGrid/Bottomgrids");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBottomGridDto>>(jsonData);
+                return View(values);
+            }
+            var errMessage = "Could not retrieve data from API. Please try again later.";
+            return Content(errMessage);
+        }
+    }
+}
