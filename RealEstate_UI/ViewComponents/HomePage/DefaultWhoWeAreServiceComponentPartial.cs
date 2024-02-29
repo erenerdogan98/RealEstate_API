3bfa42e39f97341eedc01bec1f2529df889395f5
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-02-29 07:30:39 +0300

    added using dto for namespace to _ViewImports.cshtml , added new Component partial for WhoWeAre , cutting for Services part and added ComponentInvokeAsnyc , and added class (ViewComponent) , ..

---
+++ RealEstate_UI/ViewComponents/HomePage/DefaultWhoWeAreServiceComponentPartial.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_UI.Dtos.WhoWeAreDtos;
+
+namespace RealEstate_UI.ViewComponents.HomePage
+{
+    public class DefaultWhoWeAreServiceComponentPartial(IHttpClientFactory httpClientFactory) : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44349/api/Service");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
