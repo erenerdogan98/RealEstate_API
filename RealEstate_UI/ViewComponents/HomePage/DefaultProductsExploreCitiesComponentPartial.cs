82f646dfb7ed3dea3387e12361a6d1005dbf435c
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-03-01 02:02:24 +0300

    Now to consume our API , PopularLocation , added Dto and it's namespace to ViewImports , write  for consume , and in View , added list model , and foreach loop to get values .. now we can consume our API for Popular locations .

--- RealEstate_UI/ViewComponents/HomePage/DefaultProductsExploreCitiesComponentPartial.cs
+++ RealEstate_UI/ViewComponents/HomePage/DefaultProductsExploreCitiesComponentPartial.cs
@@ -1,13 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_UI.Dtos.PopularLocationDtos;
 
 
 namespace RealEstate_UI.ViewComponents.HomePage
 {
-    public class DefaultProductsExploreCitiesComponentPartial : ViewComponent
+    public class DefaultProductsExploreCitiesComponentPartial(IHttpClientFactory httpClientFactory) : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+
+        public async Task<IViewComponentResult> InvokeAsync()
         {
-           return View();
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44349/api/PopularLocation/popularlocations");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultPopularLocationDto>>(jsonData);
+                return View(values);
+            }
+            var errMessage = "Could not retrieve data from API. Please try again later.";
+            return Content(errMessage);
         }
     }
 }
