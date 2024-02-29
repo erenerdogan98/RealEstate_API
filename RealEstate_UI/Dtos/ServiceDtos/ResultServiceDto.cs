152175fc26b6d88883f2983e6a510d418d550236
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-03-01 02:54:47 +0300

    to consume API for Testimonail (Get method) added ..

---
+++ RealEstate_UI/Dtos/ServiceDtos/ResultServiceDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RealEstate_UI.Dtos.ServiceDtos
+{
+    public class ResultServiceDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool Status { get; set; }
+    }
+}
--- RealEstate_UI/ViewComponents/HomePage/DefaultTestimonialComponentPartial.cs
+++ RealEstate_UI/ViewComponents/HomePage/DefaultTestimonialComponentPartial.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_UI.Dtos.TestimonialDtos;
 
 namespace RealEstate_UI.ViewComponents.HomePage
 {
-    public class DefaultTestimonialComponentPartial : ViewComponent
+    public class DefaultTestimonialComponentPartial(IHttpClientFactory httpClientFactory) : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+        public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View();
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44349/api/Testimonial/testimonials");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+                return View(values);
+            }
+            var errMessage = "Could not retrieve data from API. Please try again later.";
+            return Content(errMessage);
         }
     }
 }
