55191124e1fcb285889e22b5e6a7ceaaf18b6372
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-03-02 03:42:09 +0300

    Added Admin controller for create layout in Index , added layout , and I did layout partial , for doing that , added View Components ..

---
+++ RealEstate_UI/ViewComponents/AdminLayout/AdminLayoutFooterComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_UI.ViewComponents.AdminLayout
+{
+    public class AdminLayoutFooterComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ RealEstate_UI/ViewComponents/AdminLayout/AdminLayoutNavbarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_UI.ViewComponents.AdminLayout
+{
+    public class AdminLayoutNavbarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
