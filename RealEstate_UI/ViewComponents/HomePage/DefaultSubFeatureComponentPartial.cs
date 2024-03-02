237b132afe15f2379740baf0ec0ffe50236e04d1
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-03-02 03:41:02 +0300

    added for SubFeature part to consume our API (only read for now) ..

--- RealEstate_UI/ViewComponents/HomePage/DefaultSubFeatureComponentPartial.cs
+++ RealEstate_UI/ViewComponents/HomePage/DefaultSubFeatureComponentPartial.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_UI.Dtos.SubFeatureDtos;
 
 namespace RealEstate_UI.ViewComponents.HomePage
 {
-    public class DefaultSubFeatureComponentPartial : ViewComponent
+    public class DefaultSubFeatureComponentPartial(IHttpClientFactory httpClientFactory) : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+        public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View();
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44349/api/SubFeature/subfeatures");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultSubFeatureDto>>(jsonData);
+                return View(values);
+            }
+            var errMessage = "Could not retrieve data from API. Please try again later.";
+            return Content(errMessage);
         }
     }
 }
