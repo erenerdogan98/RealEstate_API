8c763ec409114d418e25a5318b2075746fe258c4
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-03-04 08:09:37 +0300

    added to consume API for BottomGrid ..

---
+++ RealEstate_UI/Controllers/BottomGridController.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_UI.Dtos.BottomGridDtos;
+using System.Text;
+
+namespace RealEstate_UI.Controllers
+{
+    [Route("{controller}")]
+    public class BottomGridController(IHttpClientFactory httpClientFactory) : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+        private readonly string dataType = "application/json";
+
+        [HttpGet("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44349/api/BottomGrid/Bottomgrids\r\n");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBottomGridDto>>(jsonData) ?? throw new InvalidDataException("...");
+                return View(values);
+            }
+            var errMessage = "Could not retrieve data from API. Please try again later.";
+            return Content(errMessage);
+        }
+
+        [HttpGet("createbottomgrid")]
+        public IActionResult CreateBottomGrid()
+        {
+            return View();
+        }
+
+        [HttpPost("createbottomgrid")]
+        public async Task<IActionResult> CreateBottomGrid(CreateBottomGridDto createBottomGrid)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var client = _httpClientFactory.CreateClient();
+                var jsonData = JsonConvert.SerializeObject(createBottomGrid);
+                StringContent stringContent = new(jsonData, Encoding.UTF8, dataType);
+
+                var responseMessage = await client.PostAsync("https://localhost:44349/api/BottomGrid/createbottomgrid\r\n", stringContent);
+
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                var errorMessage = $"Failed to Create . Status code: {responseMessage.StatusCode}.";
+                return Content(errorMessage);
+
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions, log them, or return an appropriate error message later will use Serilog 
+                var errorMessage = $"An error occurred: {ex.Message}";
+                return Content(errorMessage);
+            }
+        }
+        [HttpGet("deletebottomgrid/{id}")]
+        public async Task<IActionResult> DeleteBottomGrid(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:44349/api/BottomGrid/Deletebottomgrid/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            var errorMessage = $"Failed to delete . Status code: {responseMessage.StatusCode}.";
+            return Content(errorMessage);
+        }
+
+        [HttpGet("updatebottomgrid")]
+        public async Task<IActionResult> UpdateBottomGrid(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:44349/api/BottomGrid/GetBottomGrid/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateBottomGridDto>(jsonData);
+                return View(values);
+            }
+            var errorMessage = $"Failed to getting a data with ID : {id}. Status code: {responseMessage.StatusCode}.";
+            return Content(errorMessage);
+        }
+
+        [HttpPost("updatebottomgrid")]
+        public async Task<IActionResult> UpdateBottomGrid(UpdateBottomGridDto updateBottomGrid)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                var client = _httpClientFactory.CreateClient();
+                var jsonData = JsonConvert.SerializeObject(updateBottomGrid);
+                StringContent stringContent = new(jsonData, Encoding.UTF8, dataType);
+                var responseMessage = await client.PutAsync("https://localhost:44349/api/BottomGrid/updatebottomgrid", stringContent);
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                var errorMessage = await responseMessage.Content.ReadAsStringAsync();
+                return Content(errorMessage);
+
+            }
+            catch (Exception ex)
+            {
+                var errorMessage = $"An error occurred: {ex.Message}";
+                return Content(errorMessage);
+            }
+        }
+    }
+}
