ea69b3b15b808404c587ddde88538d9592b1a44d
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-03-05 03:11:30 +0300

    Added Dashboard controller and View for Index , I do Index view to partials for clean code , added ViewComponent classes and its Default views , to consume API ..

---
+++ RealEstate_UI/ViewComponents/Dashboard/DashboardLastFiveProductsComponentPartial.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_UI.Dtos.ProductDtos;
+
+namespace RealEstate_UI.ViewComponents.Dashboard
+{
+    public class DashboardLastFiveProductsComponentPartial(IHttpClientFactory httpClientFactory) : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44349/api/Product/LastFiveProducts");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var lastFiveProducts = JsonConvert.DeserializeObject<List<ResultLastFiveProductsDto>>(jsonData) ?? throw new InvalidDataException("...");
+                return View(lastFiveProducts);
+            }
+            var errMessage = "Could not retrieve data from API. Please try again later.";
+            return Content(errMessage);
+        }
+    }
+}
---
+++ RealEstate_UI/ViewComponents/Dashboard/DashboardStatisticComponentPartial.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstate_UI.ViewModels;
+
+namespace RealEstate_UI.ViewComponents.Dashboard
+{
+    public class DashboardStatisticComponentPartial(IHttpClientFactory httpClientFactory) : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+        private const string GeneralErrorMessage = "An error occurred while fetching statistics. Please try again later.";
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var viewModel = new StatisticsViewModel();
+            await GetStatistic("ProductCount", x => viewModel.ProductCount = ParseInt(x));
+            await GetStatistic("EmployeeByMaxProduct", x => viewModel.EmployeeNameByMaxProduct = x);
+            await GetStatistic("DifferentCityCounts", x => viewModel.DifferentCityCounts = ParseInt(x));
+            await GetStatistic("AveragePriceRented", x => viewModel.AveragePriceRented = ParseDecimal(x));
+            return View(viewModel);
+        }
+        private async Task GetStatistic(string apiEndpoint, Action<string> setProperty)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:44349/api/Statistics/{apiEndpoint}");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                setProperty(jsonData);
+            }
+            ModelState.AddModelError("", GeneralErrorMessage);
+        }
+
+        private static int ParseInt(string value)
+        {
+            return int.TryParse(value, out var result) ? result : 0;
+        }
+
+        private static decimal ParseDecimal(string value)
+        {
+            return decimal.TryParse(value, out var result) ? result : 0;
+        }
+    }
+}
