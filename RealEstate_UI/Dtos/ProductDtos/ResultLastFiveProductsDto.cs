69c7af9f75f951864ba99bc08b9c7a95e019fab9
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-03-05 03:13:10 +0300

    ...

---
+++ RealEstate_UI/Dtos/ProductDtos/ResultLastFiveProductsDto.cs
@@ -0,0 +1,13 @@
+﻿namespace RealEstate_UI.Dtos.ProductDtos
+{
+    public class ResultLastFiveProductsDto
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public decimal Price { get; set; }
+        public string? City { get; set; }
+        public string? District { get; set; }
+        public string? Name { get; set; }
+        public DateTime AdvertisementDate { get; set; }
+    }
+}
