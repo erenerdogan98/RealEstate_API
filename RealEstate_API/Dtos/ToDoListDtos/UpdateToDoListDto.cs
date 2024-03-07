a5494c42f3237e0ece3982396d22baf86e3e8cf5
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-03-07 16:56:27 +0300

    ...

---
+++ RealEstate_API/Dtos/ToDoListDtos/UpdateToDoListDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RealEstate_API.Dtos.ToDoListDtos
+{
+    public class UpdateToDoListDto
+    {
+        public int Id { get; set; }
+        public string? Description { get; set; }
+        public bool Status { get; set; }
+    }
+}
